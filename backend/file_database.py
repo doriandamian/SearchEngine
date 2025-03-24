bdc99b107e564d30ab28a20ea1351b0cfe665482
Dorian Damian <dorian.damian355@gmail.com>
2025-03-24 15:24:59 +0200

    Backend: Implement search query for database

--- backend/file_database.py
+++ backend/file_database.py
@@ -2,7 +2,7 @@ import sqlite3
 
 class FileDatabase:
     def __init__(self, dbName = "database.db"):
-        self.conn = sqlite3.connect("../database/" + dbName)
+        self.conn = sqlite3.connect("../database/" + dbName, check_same_thread=False)
 
     def insertFile(self, fileData):
         cursor = self.conn.cursor()
@@ -11,3 +11,19 @@ class FileDatabase:
             VALUES (?, ?, ?, ?, ?, ?, ?)
         ''', fileData)
         self.conn.commit()
+
+    def searchFiles(self, query):
+        cursor = self.conn.cursor()
+
+        cursor.execute("""
+        SELECT f.path, f.title, f.extension, f.content, f.created_at, f.modified_at, f.size
+        FROM files AS f
+        INNER JOIN files_fts AS fts
+        ON f.id = fts.id
+        WHERE fts.title LIKE ? OR fts.content LIKE ?
+        ORDER BY bm25(files_fts)
+        LIMIT 20;
+        """, (f"%{query}%", f"%{query}%"))
+
+        results = cursor.fetchall()
+        return results
