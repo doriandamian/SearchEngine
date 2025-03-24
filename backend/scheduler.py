f4e4d54710febf1e280d5ab58a3c58f2e4b4cdb3
Dorian Damian <dorian.damian355@gmail.com>
2025-03-24 12:08:57 +0200

    Logger: Added log calls

--- backend/scheduler.py
+++ backend/scheduler.py
@@ -1,20 +1,20 @@
+from logger import Logger
+
 from file_database import FileDatabase
 from crawler import FileCrawler
 
-import time
-
 def indexFiles():
-    startTime = time.time()
-
     crawler = FileCrawler()
     db = FileDatabase()
+    logger = Logger()
+    fileCount = 0
 
-    for fileData in crawler.crawl():
+    for fileData in crawler.crawl(logger):
         if fileData:
             db.insertFile(fileData)
-    
-    endTime = time.time()
-    runTime = endTime - startTime
-    print(f"Indexing complete in {runTime:.2f} seconds")
+            fileCount += 1
+
+    logger.addLog(f"All files indexed succesfully.")
+    logger.finish(fileCount)
 
 indexFiles()
