3e4008772784a088b1d32cb6d2d468716f8c1a72
Dorian Damian <dorian.damian355@gmail.com>
2025-04-26 19:09:24 +0300

    Backend: Add ranking system based on path lenght, last modify and bm25

--- backend/file_database.py
+++ backend/file_database.py
@@ -24,17 +24,28 @@ class FileDatabase:
                 params.append(f"%{value}%")
             wheres.append(f"({' AND '.join(temp)})")
         
+        # Formula:
+        # Shorter path
+        # More recent modified_at
+        # Higher bm25
+        scoreFormula = f"""
+            (
+                (1000.0 / LENGTH(f.path)) + 
+                (strftime('%s', 'now') - strftime('%s', f.modified_at)) / -86400.0
+            ) - bm25(files_fts)
+        """
+
         if wheres:
             whereString = ' AND '.join(wheres)
         else:
             whereString = "1=1"
 
         cursor.execute(f"""
-        SELECT f.path, f.title, f.extension, f.content, f.created_at, f.modified_at, f.size
+        SELECT f.path, f.title, f.extension, f.content, f.created_at, f.modified_at, f.size, {scoreFormula} as score
         FROM files AS f
         INNER JOIN files_fts AS fts ON f.id = fts.id
         WHERE {whereString}
-        ORDER BY bm25(files_fts)
+        ORDER BY score DESC
         LIMIT 20;
         """, params)
 
